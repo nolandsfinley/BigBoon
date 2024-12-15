e29cc819245d03902a3b75dbe6b792eab8ac8509
nolandsfinley <147907708+nolandsfinley@users.noreply.github.com>
2024-12-15 21:24:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// แสดงข้อความใน Console
+console.log("Welcome to My Website!");
+
+// เมื่อโหลดหน้าเสร็จ ให้แสดงข้อความแจ้งเตือน
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Hello! Welcome to my website.");
+});
